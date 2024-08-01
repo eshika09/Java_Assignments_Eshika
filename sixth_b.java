bc2e5fbfa1d9b92a93a0bc609aa5deb4fda48182
eshika09 <153345896+eshika09@users.noreply.github.com>
2024-08-01 23:42:10 +0530

    Add files via upload

---
+++ sixth_b.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class sixth_b {
+    public static void evenodd(int n){
+        if(n%2==0){
+            System.out.println("Even number");
+        }else{
+            System.out.println("Odd number");
+        }
+    }
+   public static void main(String [] args){
+    int n=7;
+    evenodd(n);
+    int b=10;
+    evenodd(b);
+   } 
+}
