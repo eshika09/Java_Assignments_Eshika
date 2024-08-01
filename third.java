0a820e92eb84ad661303f2220e3067955b156e49
eshika09 <153345896+eshika09@users.noreply.github.com>
2024-08-01 21:56:04 +0530

    Add files via upload

---
+++ third.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class third {
+    public static void main(String[] args){
+     Object a=10;
+     Object b='&';
+     Object c = "Eshika";
+     Object d=5.7;
+     Object e=true;
+
+     System.out.println(a.getClass().getSimpleName());
+     System.out.println(b.getClass().getSimpleName());
+     System.out.println(c.getClass().getSimpleName());
+     System.out.println(d.getClass().getSimpleName());
+     System.out.println(e.getClass().getSimpleName());
+    }
+    
+}
