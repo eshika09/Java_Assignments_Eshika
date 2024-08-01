ffb27002f08f8626b946faf51e24f4dbf7ee0852
eshika09 <153345896+eshika09@users.noreply.github.com>
2024-08-01 22:02:35 +0530

    Add files via upload

---
+++ fourth.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class fourth {
+    public static void main(String[] args){
+        double d=5.678;
+        int c=(int) d;
+        System.out.println("Original double value: " +d);
+        System.out.println("Converted value(double to int): " +c);
+    }
+}
