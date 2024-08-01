d84f8f24a33ebe84b9757e7c1f7d9102f7d67f56
eshika09 <153345896+eshika09@users.noreply.github.com>
2024-08-01 23:04:19 +0530

    Add files via upload

--- fourth.java
+++ fourth.java
@@ -3,7 +3,11 @@ public class fourth {
     public static void main(String[] args){
         double d=5.678;
         int c=(int) d;
+        char a='*';
+        float f=(float)a;
         System.out.println("Original double value: " +d);
         System.out.println("Converted value(double to int): " +c);
+        System.out.println("Original char value: " +a);
+        System.out.println("Converted value(char to float): " +f);
     }
 }
