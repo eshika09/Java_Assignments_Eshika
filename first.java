fc04693aaba1efcf0128fbfccc60b717122c53cc
eshika09 <153345896+eshika09@users.noreply.github.com>
2024-08-01 20:10:01 +0530

    Add files via upload

--- first.java
+++ first.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class first{
-    static void main(String args[]){
+   public  static void main(String args[]){
         
         System.out.println("Welcome");
     }
