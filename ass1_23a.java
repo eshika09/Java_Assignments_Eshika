c78b38e2f6d4e3f78914913872782286a81ac0c1
eshika09 <153345896+eshika09@users.noreply.github.com>
2024-10-21 23:23:49 +0530

    Add files via upload

---
+++ ass1_23a.java
@@ -0,0 +1,13 @@
+public class ass1_23a {
+    public static void main(String[] args) {
+        int count=1;
+        for(int i=1; i<=3; i++){
+            for(int j=1; j<=2*i-1; j++){
+                System.out.print(count + " ");
+                count++;
+            }System.out.println();
+        }
+    }
+}
+
+
